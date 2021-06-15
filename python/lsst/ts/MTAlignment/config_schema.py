9a9220ea7c29c1398e7cfcfd6d5170c09d5c9330
Colin Winslow <cwinslow@lsst.org>
2021-06-15 15:51:04 -0700

    adds many more items to config file schema

--- python/lsst/ts/MTAlignment/config_schema.py
+++ python/lsst/ts/MTAlignment/config_schema.py
@@ -20,5 +20,45 @@ properties:
     description: port of T2SA instance.
     type: number
     default: 50000
+  num_iterations:
+    description: number of times to repeat measurements of a point group
+    type: number
+    default: 1
+  num_samples:
+    description: number of times to sample each point within a single visit
+    type: number
+    default: 1
+  randomize_points:
+    description: if true, visit the SMRs in a random order
+    type: boolean
+    default: false
+  station_lock:
+    description: if true, prevents SpatialAnalyzer from automatically jumping stations if it detects that the tracker has drifted
+    type: boolean
+    default: false
+  rms_tolerance:
+    description: RMS least squares tolerance in mm
+    type: number
+    default: 0.05
+  max_tolerance:
+    description: maximum absolute tolerance  in mm
+    type: number
+    default: 1.0
+  two_face_tolerance:
+    description: maximum allowed divergence when measuring the same point using the trackers two different "facings" in decimal degrees
+    type: number
+    default: 0.001
+  rms_drift_tolerance:
+    description: RMS least squares tolerance in mm
+    type: number
+    default: 0.05
+  max_drift_tolerance:
+    description: maximum absolute tolerance  in mm
+    type: number
+    default: 1.0
+  power_lock:
+    description: if true, enables the tracker’s camera (used to help search for SMRs). 
+    type: boolean
+    default: true
 """
 )

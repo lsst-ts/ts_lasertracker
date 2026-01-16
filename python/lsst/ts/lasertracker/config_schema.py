eebb06e2f8856d640f18c106bb542feab1a199de
Tiago Ribeiro <tiago.ribeiro@noirlab.edu>
2026-01-16 10:08:51 -0700

    Update configuration schema to add a LUT coefficients parameters for the Camera and M2 hexapods.

--- python/lsst/ts/lasertracker/config_schema.py
+++ python/lsst/ts/lasertracker/config_schema.py
@@ -28,7 +28,7 @@ CONFIG_SCHEMA = yaml.safe_load(
 $schema: http://json-schema.org/draft-07/schema#
 $id: https://github.com/lsst-ts/ts_lasertracker/blob/master/schema/alignment.yaml
 # title must end with one or more spaces followed by the schema version, which must begin with "v"
-title: LaserTracker v2
+title: LaserTracker v3
 description: Schema for LaserTracker CSC configuration files
 type: object
 properties:
@@ -149,6 +149,140 @@ properties:
         single_point_measurement_profile:
           description: Name of Spatial Analyzer measurement profile.
           type: string
+        targets_lut_coeffs:
+          description: Target-specific LUT.
+          type: object
+          properties:
+            CAM:
+              description: Camera LUT
+              type: object
+              properites:
+                elevation_rotation_coeffs:
+                description: >-
+                  Elevation/rotation compensation coefficients.
+                  Rows are coefficients for x, y, z (um), u, v, w (deg).
+                  The first index corresponds to powers of elevation and
+                  the second index corresponds to powers of rotation.
+                  Values are the coefficients in equation C_0_0
+                  + C_0_1 * el + C_0_2 * el**2 + ... + C_1_0 * rot
+                  + C_2_0 * rot**2 + ... + C_1_1 * el * rot
+                  + C_1_2 * el**2 * rot + ... + C_2_1 * el * rot**2 + ...
+                  where el is in deg.
+                  Compensated value = uncompensated (user-specified) value
+                    + elevation compensation
+                    + azimuth compensation
+                    + rotation compensation
+                    + temperature compensation.
+                type: array
+                minItems: 6
+                maxItems: 6
+                items:
+                  type: array
+                  minItems: 1
+                  items:
+                    type: array
+                    minItems: 1
+                    items:
+                      type: number
+                azimuth_coeffs:
+                description: >-
+                  Azimuth compensation coefficients.
+                  Rows are coefficients for x, y, z (um), u, v, w (deg).
+                  Values are the coefficients in equation C0 + C1 az + C2 az^2 + ...,
+                  where az is in deg.
+                type: array
+                minItems: 6
+                maxItems: 6
+                items:
+                  type: array
+                  minItems: 1
+                  items:
+                    type: number
+                temperature_coeffs:
+                description: >-
+                  Temperature compensation coefficients.
+                  Rows are coefficients for x, y, z (um), u, v, w (deg).
+                  Values are the coefficients in equation C0 + C1 temp + C2 temp^2 + ...,
+                  where temp is in C.
+                type: array
+                minItems: 6
+                maxItems: 6
+                items:
+                  type: array
+                  minItems: 1
+                  items:
+                    type: number
+            M2:
+              description: M2 LUT
+              type: object
+              properites:
+                elevation_rotation_coeffs:
+                description: >-
+                  Elevation/rotation compensation coefficients.
+                  Rows are coefficients for x, y, z (um), u, v, w (deg).
+                  The first index corresponds to powers of elevation and
+                  the second index corresponds to powers of rotation.
+                  Values are the coefficients in equation C_0_0
+                  + C_0_1 * el + C_0_2 * el**2 + ... + C_1_0 * rot
+                  + C_2_0 * rot**2 + ... + C_1_1 * el * rot
+                  + C_1_2 * el**2 * rot + ... + C_2_1 * el * rot**2 + ...
+                  where el is in deg.
+                  Compensated value = uncompensated (user-specified) value
+                    + elevation compensation
+                    + azimuth compensation
+                    + rotation compensation
+                    + temperature compensation.
+                type: array
+                minItems: 6
+                maxItems: 6
+                items:
+                  type: array
+                  minItems: 1
+                  items:
+                    type: array
+                    minItems: 1
+                    items:
+                      type: number
+                azimuth_coeffs:
+                description: >-
+                  Azimuth compensation coefficients.
+                  Rows are coefficients for x, y, z (um), u, v, w (deg).
+                  Values are the coefficients in equation C0 + C1 az + C2 az^2 + ...,
+                  where az is in deg.
+                type: array
+                minItems: 6
+                maxItems: 6
+                items:
+                  type: array
+                  minItems: 1
+                  items:
+                    type: number
+                temperature_coeffs:
+                description: >-
+                  Temperature compensation coefficients.
+                  Rows are coefficients for x, y, z (um), u, v, w (deg).
+                  Values are the coefficients in equation C0 + C1 temp + C2 temp^2 + ...,
+                  where temp is in C.
+                type: array
+                minItems: 6
+                maxItems: 6
+                items:
+                  type: array
+                  minItems: 1
+                  items:
+                    type: number
+        min_temperature:
+          description: >-
+            Minimum temperatures (C) for which the temperature model is valid.
+            Below this temperature, terms above the first order are ignored;
+            see RangedPolynomial for details.
+          type: number
+        max_temperature:
+          description: >-
+            Maximum temperatures (C) for which the temperature model is valid.
+            Above this temperature, terms above the first order are ignored;
+            see RangedPolynomial for details.
+          type: number
       required:
         - sal_index
         - t2sa_host
